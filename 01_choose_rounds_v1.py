2a7f9b10f718778d666162e7758f79cb9a6740b6
AnaheraR <129803740+AnaheraR@users.noreply.github.com>
2023-06-07 09:10:48 +1200

    Add files via upload

---
+++ 01_choose_rounds_v1.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from functools import partial  # To import unwanted windows
+
+
+class ChooseRounds:
+
+    def __init__(self):
+        button_fg = "#FFFFFF"
+        button_font = ("Arial", "13", "bold")
+
+        # Set up GUI frame
